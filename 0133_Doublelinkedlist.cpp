3d427d0cbfca1bb1866a562897e0992bb776f88b
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-04-30 11:34:51 +0700

    Doublelinkedlist 5

--- 0133_Doublelinkedlist.cpp
+++ 0133_Doublelinkedlist.cpp
@@ -35,8 +35,20 @@ public:
         {
             if (START != NULL && nim == START->noMhs)
             {
-                
+               cout << "\nDuplicate number not allowed" << endl;
+               return; 
             }
+
+        newNode->next = START;
+
+        if (START != NULL)
+            START->prev = newNode;
+        
+        newNode->prev =NULL;
+        
+        START = newNode;
+        return;    
         }
+        
     }
 }
